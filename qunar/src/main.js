3d0848f644d350512afc26a8d254bfcc46f5b8ed
bigfish <thebigfish@foxmail.com>
2019-02-24 18:11:51 +0800

    初始化qunar项目为空

--- qunar/src/main.js
+++ qunar/src/main.js
@@ -3,13 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
-
+fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
